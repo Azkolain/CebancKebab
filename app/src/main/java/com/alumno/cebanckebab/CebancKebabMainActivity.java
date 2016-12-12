30babe1b9d18db4715b91578774dea48a02b7384
Azkolain <asierazko@gmail.com>
2016-12-12 11:10:58 +0100

    Initial commit

---
+++ app/src/main/java/com/alumno/cebanckebab/CebancKebabMainActivity.java
@@ -0,0 +1,13 @@
+package com.alumno.cebanckebab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CebancKebabMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cebanc_kebab_main);
+    }
+}
