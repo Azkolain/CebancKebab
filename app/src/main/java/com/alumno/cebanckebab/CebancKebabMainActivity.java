5f4a57aa6be12e18de28f12067773c91ce30d261
alvarolinoo <alvarolino@ikaslagun.org>
2016-12-15 09:14:45 +0100

    kaixo

--- app/src/main/java/com/alumno/cebanckebab/CebancKebabMainActivity.java
+++ app/src/main/java/com/alumno/cebanckebab/CebancKebabMainActivity.java
@@ -3,11 +3,23 @@ package com.alumno.cebanckebab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CebancKebabMainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
+public class CebancKebabMainActivity extends AppCompatActivity {
+    private GoogleMap mapa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cebanc_kebab_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+
+    public void onMapReady(GoogleMap map) {
+        mapa = map;
     }
 }
