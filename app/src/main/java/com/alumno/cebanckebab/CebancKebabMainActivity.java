35f8103954633ccb9de700dad3ee2c862bac9146
alvarolinoo <alvarolino@ikaslagun.org>
2016-12-19 12:19:49 +0100

    zepitxoro

--- app/src/main/java/com/alumno/cebanckebab/CebancKebabMainActivity.java
+++ app/src/main/java/com/alumno/cebanckebab/CebancKebabMainActivity.java
@@ -8,21 +8,19 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CebancKebabMainActivity extends AppCompatActivity {
+
+public class CebancKebabMainActivity extends AppCompatActivity implements OnMapReadyCallback{
     private GoogleMap mapa;
     private FirstMapFragment mFirstMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cebanc_kebab_main);
         getSupportActionBar().setBackgroundDrawable(
@@ -34,19 +32,20 @@ public class CebancKebabMainActivity extends AppCompatActivity {
                 .add(R.id.map, mFirstMapFragment)
                 .commit();
 
+        // Registrar escucha onMapReadyCallback
+        mFirstMapFragment.getMapAsync(this);
     }
-        public void onMapReady(GoogleMap googleMap) {
-        LatLng cali = new LatLng(3.4383, -76.5161);
-        googleMap.addMarker(new MarkerOptions()
-                .position(cali)
-                .title("Cali la Sucursal del cielo"));
 
-        CameraPosition cameraPosition = CameraPosition.builder()
-                .target(cali)
-                .zoom(10)
-                .build();
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = new LatLng(36.2048, 138.2529);
 
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        MarkerOptions markerOptions =
+                new MarkerOptions()
+                        .position(latLng)
+                        .title("Japón")
+                        .snippet("Primer ministro: Shinzō Abe");
+
+        Marker marker = googleMap.addMarker(markerOptions);
     }
 
     public void onClickSalir(View view) {
